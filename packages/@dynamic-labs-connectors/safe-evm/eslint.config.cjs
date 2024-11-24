af5ed7f2945436dd6963aa04e1e21da4dd0e2ca6
Yoni Goldberg <yoni@dynamic.xyz>
2024-11-24 10:42:56 -0500

    chore: update the pnpm-lock file (#48)
    Also temp disabled the *.json dependency checks because it wasn't
    handling peer dependencies properly

--- packages/@dynamic-labs-connectors/safe-evm/eslint.config.cjs
+++ packages/@dynamic-labs-connectors/safe-evm/eslint.config.cjs
@@ -6,7 +6,7 @@ module.exports = [
     files: ['**/*.json'],
     rules: {
       '@nx/dependency-checks': [
-        'error',
+        'warn',
         {
           ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'],
         },
